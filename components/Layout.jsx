897119cb1b47ec04a9fe16bf73930fca0cd1467c
aridepai17 <advaithdepai26@gmail.com>
2025-07-16 02:01:42 +0530

    reafactor: Renamed the app to Housini in tab instead of REAL ESTATE

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>REAL ESTATE</title>
+        <title>Housini</title>
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
